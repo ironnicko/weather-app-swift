56ae6f8688541e8363a0be8d6a590f5d9f8fe728
ironnicko <nickonicko779@gmail.com>
2022-06-11 23:37:56 +0530

    Initial Commit

---
+++ networkapp/networkappApp.swift
@@ -0,0 +1,17 @@
+//
+//  networkappApp.swift
+//  networkapp
+//
+//  Created by Nikhil Ivannan on 11/06/22.
+//
+
+import SwiftUI
+
+@main
+struct networkappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
